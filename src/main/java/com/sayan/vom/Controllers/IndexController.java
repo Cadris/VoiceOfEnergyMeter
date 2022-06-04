7351a92c32e7a4b4d87eb29352ae216f4ef09673
Cadris <cadrissilver@gmail.com>
2022-06-05 02:09:39 +0530

    Data Auto Update to the Screen from the Database

--- src/main/java/com/sayan/vom/Controllers/IndexController.java
+++ src/main/java/com/sayan/vom/Controllers/IndexController.java
@@ -1,12 +1,16 @@
 package com.sayan.vom.Controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sayan.vom.Entities.EnergyData;
 import com.sayan.vom.Repositories.EnergyDataRepository;
@@ -43,6 +47,23 @@ public class IndexController {
 		
 		model.addAttribute("dataList", dataList);
 		return "dataChart";
+	}
+	
+	@GetMapping("/home/autoReload")
+	@ResponseBody
+	@CrossOrigin
+	public Map autoReload(Model model) {
+		
+		EnergyData ed = edRepo.getLastEntry();
+		System.out.println("IN Server");
+		
+		model.addAttribute("value", ed.getPower());
+		model.addAttribute("valueCost", calcService.getPrice(ed.getPower()));
+		
+		Map<String, Object> map = new HashMap<String,Object>();
+		map.put("success",true);
+		
+		return map;
 	}
 	
 
