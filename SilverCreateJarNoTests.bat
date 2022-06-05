44d93babb0ca96acd09155e2e407cb44af5d42ad
Cadris <cadrissilver@gmail.com>
2022-06-05 12:04:23 +0530

    prot

--- SilverCreateJarNoTests.bat
+++ SilverCreateJarNoTests.bat
@@ -1,1 +1,5 @@
+echo off
+pause
 mvn package -DskipTests
+pause
+xcopy ".\target\vom-0.0.1-SNAPSHOT.jar" ".\Application\vom-0.0.1-SNAPSHOT.jar"
---
+++ dietJreCreator.bat
@@ -0,0 +1,2 @@
+jlink --add-modules java.base,java.logging,java.xml,jdk.unsupported,java.sql,java.naming,java.desktop,java.management,java.security.jgss,java.instrument --output ./Application/dietjre
+pause
